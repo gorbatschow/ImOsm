61d1d3b2008c7ab360d7292040889aa695931724
gorbatschow <gorbatschow.r@gmail.com>
2022-12-17 04:45:17 +0300

    fixed zoom level computation

--- ImOsmWidget.cpp
+++ ImOsmWidget.cpp
@@ -6,48 +6,56 @@
 ImOsmWidget::ImOsmWidget() {}
 
 void ImOsmWidget::paint() {
-  ImGui::Begin("OsmPlotWidget");
-
   if (ImPlot::BeginPlot("##OsmPlot", {-1, -1},
                         ImPlotFlags_Equal | ImPlotFlags_NoLegend)) {
-    ImPlot::SetupAxis(ImAxis_Y1, NULL, ImPlotAxisFlags_Invert);
 
-    const auto plotLimits = ImPlot::GetPlotLimits(ImAxis_X1, ImAxis_Y1);
-    const auto plotSizePx = ImPlot::GetPlotSize();
-    auto Z = float(1 << _zoom);
-    _plotTileSizePx = plotSizePx.x / plotLimits.Size().x / Z;
-    if (_plotTileSizePx > _tileSizePx * _zoomCoef) {
-      Z = float(1 << ++_zoom);
-    } else if (_plotTileSizePx < _tileSizePx) {
-      Z = float(1 << --_zoom);
-    }
-    _zoom = std::clamp(_zoom, 0, LimZoom);
+    ImPlot::SetupAxis(ImAxis_X1, NULL, ImPlotAxisFlags_NoInitialFit);
+    ImPlot::SetupAxis(ImAxis_Y1, NULL,
+                      ImPlotAxisFlags_Invert | ImPlotAxisFlags_NoInitialFit);
+    ImPlot::SetupFinish();
 
-    _minLon =
-        std::clamp(x2lon(float(plotLimits.X.Min) * Z, _zoom), -LimLon, LimLon);
-    _maxLon =
-        std::clamp(x2lon(float(plotLimits.X.Max) * Z, _zoom), -LimLon, LimLon);
-    _minLat =
-        std::clamp(y2lat(float(plotLimits.Y.Min) * Z, _zoom), -LimLat, LimLat);
-    _maxLat =
-        std::clamp(y2lat(float(plotLimits.Y.Max) * Z, _zoom), -LimLat, LimLat);
+    const auto plotLims = ImPlot::GetPlotLimits(ImAxis_X1, ImAxis_Y1);
+    const auto plotSize = ImPlot::GetPlotSize();
 
-    _minTX = lon2tx(_minLon, _zoom);
-    _maxTX = lon2tx(_maxLon, _zoom);
-    _minTY = lat2ty(_minLat, _zoom);
-    _maxTY = lat2ty(_maxLat, _zoom);
+    _pixelsX = plotSize.x;
+    _pixelsY = plotSize.y;
+
+    _minX = plotLims.X.Min;
+    _maxX = plotLims.X.Max;
+    _minY = plotLims.Y.Min;
+    _maxY = plotLims.Y.Max;
+    _rangeX = abs(_maxX - _minX);
+    _rangeY = abs(_maxY - _minY);
+
+    _resX = _pixelsX / _rangeX;
+    _resY = _pixelsY / _rangeY;
+    _zoom = std::clamp(floor(log2(_resX / _tilePixels)), 0.0, 18.0);
+    _tilesNum = (1 << _zoom);
+    _tileSize = 1.0 / _tilesNum;
+
+    _minLon = std::clamp(x2lon(_minX * _tilesNum, _zoom), -LimLon, LimLon);
+    _maxLon = std::clamp(x2lon(_maxX * _tilesNum, _zoom), -LimLon, LimLon);
+    _minLat = std::clamp(y2lat(_minY * _tilesNum, _zoom), -LimLat, LimLat);
+    _maxLat = std::clamp(y2lat(_maxY * _tilesNum, _zoom), -LimLat, LimLat);
+
+    _minTX = lon2tx(std::clamp(_minLon, -LimLon, LimLon), _zoom);
+    _maxTX = lon2tx(std::clamp(_maxLon, -LimLon, LimLon), _zoom);
+    _minTY = lat2ty(std::clamp(_minLat, -LimLat, LimLat), _zoom);
+    _maxTY = lat2ty(std::clamp(_maxLat, -LimLat, LimLat), _zoom);
 
     _loader.beginLoad(_zoom, _minTX, _maxTX, _minTY, _maxTY);
 
     const ImVec2 uv0(0, 1), uv1(1, 0);
     const ImVec4 tint(1, 1, 1, 1);
-    ImVec2 bmin, bmax;
+    ImVec2 bmin{float(_minTX), float(_minTY)};
+    ImVec2 bmax{float(_maxTX), float(_maxTY)};
+
     for (auto x = _minTX; x != _maxTX + 1; ++x) {
-      bmin.x = float(x) / Z;
-      bmax.x = float(x + 1) / Z;
+      bmin.x = float(x) * _tileSize;
+      bmax.x = float(x + 1) * _tileSize;
       for (auto y = _minTY; y != _maxTY + 1; ++y) {
-        bmin.y = float(y) / Z;
-        bmax.y = float(y + 1) / Z;
+        bmin.y = float(y) * _tileSize;
+        bmax.y = float(y + 1) * _tileSize;
         ImPlot::PlotImage("##", _loader.tileAt(_zoom, x, y), bmin, bmax, uv0,
                           uv1, tint);
       }
@@ -55,6 +63,4 @@ void ImOsmWidget::paint() {
 
     ImPlot::EndPlot();
   }
-
-  ImGui::End();
 }
--- ImOsmWidget.h
+++ ImOsmWidget.h
@@ -5,21 +5,53 @@
 class ImOsmWidget {
 public:
   ImOsmWidget();
+
   void paint();
+
+  inline void getBounds(double &minLon, double &maxLon, double &minLat,
+                        double &maxLat) const;
+  inline void getBounds(int &minTX, int &maxTX, int &minTY, int &maxTY) const;
+  inline double minLon() const { return _minLon; }
+  inline double maxLon() const { return _maxLon; }
+  inline double minLat() const { return _minLat; }
+  inline double maxLat() const { return _maxLat; }
+  inline int zoom() const { return int(_zoom); }
 
 private:
   constexpr static const float LimLat{85.0};
   constexpr static const float LimLon{179.9};
   constexpr static const int LimZoom{18};
 
-  float _tileSizePx{256.0};
-  float _plotTileSizePx{};
-  float _zoomCoef{2.0};
+  float _tilePixels{256.0};
+  float _tileSize{};
   float _minLon{-LimLon}, _maxLon{LimLon};
   float _minLat{-LimLat}, _maxLat{LimLat};
+  float _minX{}, _maxX{};
+  float _minY{}, _maxY{};
   int _minTX{}, _maxTX{};
   int _minTY{}, _maxTY{};
+  float _pixelsX{}, _pixelsY{};
+  float _rangeX{}, _rangeY{};
+  float _resX{}, _resY{};
+  int _tilesNum{};
   int _zoom{0};
+  bool _setBounds{true};
 
   OsmTileLoader _loader;
 };
+
+inline void ImOsmWidget::getBounds(double &minLon, double &maxLon,
+                                   double &minLat, double &maxLat) const {
+  minLon = _minLon;
+  maxLon = _maxLon;
+  minLat = _minLat;
+  maxLat = _maxLat;
+}
+
+inline void ImOsmWidget::getBounds(int &minTX, int &maxTX, int &minTY,
+                                   int &maxTY) const {
+  minTX = _minTX;
+  maxTX = _maxTX;
+  minTY = _minTY;
+  maxTY = _maxTY;
+}
