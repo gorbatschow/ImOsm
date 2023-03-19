7755542f3347df6df4c8fb115dea0491f6df3692
gorbatschow <gorbatschow.r@gmail.com>
2023-03-19 18:04:13 +0300

    moved CMakeLists

--- ImOsmTile.h
+++ ImOsmTile.h
@@ -7,26 +7,12 @@
 namespace ImOsm {
 class Tile {
 public:
-  Tile(int z, int x, int y, const std::vector<std::byte> &blob, bool ok = true)
-      : _z{z}, _x{x}, _y{y}, _blob{blob}, _ok{ok} {}
+  Tile(int z, int x, int y, const std::vector<std::byte> &blob, bool ok = true);
 
-  void loadTexture() {}
+  void loadTexture();
 
   void saveFile(const std::string &dirName,
-                const std::string &ext = ".png") const {
-    std::ostringstream pathmaker;
-    pathmaker << dirName << '/' << _z << '/' << _x;
-    if (!std::filesystem::exists(pathmaker.str())) {
-      std::filesystem::create_directories(pathmaker.str());
-    }
-    pathmaker << '/' << _z << '-' << _x << '-' << _y << ext;
-    std::ofstream filemaker(pathmaker.str().c_str(),
-                            std::fstream::out | std::fstream::binary);
-    if (filemaker) {
-      filemaker.write(reinterpret_cast<const char *>(_blob.data()),
-                      _blob.size());
-    }
-  }
+                const std::string &ext = ".png") const;
 
 private:
   int _z{}, _x{}, _y{};
